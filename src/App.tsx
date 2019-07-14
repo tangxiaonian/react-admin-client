1626aced816c7ea0fc7b8af8ba2c0a75d59a6784
tang <2684270465@qq.com>
2019-07-14 11:49:18 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button } from "antd";
+
+interface HelloProps {
+    compiler: string;
+    framework: string;
 }
+
+class App extends React.Component<HelloProps,{}>{
+
+    render(){
+
+        return (
+            <h1>
+                Hello from { this.props.compiler } and { this.props.framework }!
+                <Button type={"primary"}>Button</Button>
+            </h1>
+        );
+
+    }
+};
 
 export default App;
